93688ad2a432b724ebd77f6bf2fd0c1f29503861
rafay-sandhu <88657320+rafay-sandhu@users.noreply.github.com>
2026-06-14 16:19:20 +0500

    Create basic HTML calculator with CSS styling. Create event listeners to update display and perform relevant calculations when a button is pressed. Remove bugs and edge cases to ensure smooth performance of calculator.

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 function add(num1, num2) {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return Number(num1) - Number(num2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return Number(num1) * Number(num2);
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    if (Number(num2) === 0){
+        return "Stop dividing by zero";
+    }
+
+    if (Number.isInteger(Number(num1) / Number(num2))){
+        return Number(num1) / Number(num2);
+    }
+    else return parseFloat((Number(num1) / Number(num2)).toFixed(3));
 }
 
 function operate(operator, num1, num2) {
@@ -22,7 +29,7 @@ function operate(operator, num1, num2) {
         case "-":
             return subtract(num1, num2);
             break;
-        case "*":
+        case "x":
             return multiply(num1, num2);
             break;
         case "/":
@@ -32,3 +39,44 @@ function operate(operator, num1, num2) {
 }
 
 let first_input, second_input, operator = "";
+let resultCheck = false;
+
+const display = document.querySelector(".display");
+const leftButton = document.querySelectorAll(".left-button");
+const rightButton = document.querySelectorAll(".right-button");
+
+leftButton.forEach(element => {
+    element.addEventListener("click", function(e) {
+        if(resultCheck === true || display.textContent === "Stop dividing by zero"){
+            display.textContent = "";
+            resultCheck = false;
+        }
+        display.textContent += e.target.textContent});
+});    
+
+rightButton.forEach(element => {
+    element.addEventListener("click", function(e) {
+        if(resultCheck === true) {
+            resultCheck = false};
+
+        if(e.target.textContent == "clr" || display.textContent === "Stop dividing by zero" || display.textContent === "") {
+            display.textContent = "";
+            return;
+        }
+
+        const temp_arr = display.textContent.split(" ")
+        if(temp_arr.length === 3){
+            const result = operate(temp_arr[1], temp_arr[0], temp_arr[2]);
+            display.textContent = "";
+            display.textContent += result;
+        }
+
+        if(e.target.textContent == "="){
+            resultCheck = true;
+            return;
+        }
+        else {
+            display.textContent += " " + e.target.textContent + " ";
+        }
+    });
+});
